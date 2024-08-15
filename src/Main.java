9b8cd8154701e40d33d133182e916a6d577b1dab
Darya <daryaazyl@yandex.ru>
2024-08-15 19:57:22 +0300

    add sout change develop 2

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Branch develop");
         System.out.println("change develop 1");
+        System.out.println("change develop 2");
     }
 }
