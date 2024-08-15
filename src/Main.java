781caba9780894ba79e0908bc499c75f0d2bcd20
Darya <daryaazyl@yandex.ru>
2024-08-15 20:22:00 +0300

    add sout release 1

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println("change develop 3");
         System.out.println("add sout hotfixes");
         System.out.println("add sout release");
+        System.out.println("add sout release 1");
     }
 }
